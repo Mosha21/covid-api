5d917a312f1e19e65f9fd42a9daccee98f97bc6f
Bernardo Moya <bernardo_mm@hotmail.com>
2021-06-01 12:26:17 -0600

    first commit

---
+++ public/js/main.js
@@ -0,0 +1,78 @@
+var width = 450;
+var height = 450;
+
+var margin = {top: 20, right: 20, bottom: 20, left:20}
+
+var radius = Math.min(width, height) / 2 - 20
+
+var svg = d3.select("#nationalities").append("svg")
+.attr("width", width + margin.right + margin.left)
+.attr("height", height + margin.top + margin.bottom)
+.append("g")
+.attr("transform", "translate(" + width / 2 + "," + height / 2 + ")")
+
+var pieSet1 = {}
+var pieSet2 = {
+	algo: 5,
+	otra: 10
+}
+
+fetch('http://localhost:3000/nationality')
+.then(result => result.json()).then(data => {
+	update(data)
+	pieSet1 = data
+})
+
+var color = d3.scaleOrdinal()
+.range(d3.schemeDark2)
+
+const update = (data) => {
+	var pie = d3.pie()
+	.value((d) => d.value)
+	.sort((a, b) => d3.ascending(a.key, b.key))
+
+	var g = svg.selectAll("path")
+	.data(pie(d3.entries(data)))
+
+	g
+	.enter()
+    .append('path')
+    .merge(g)
+	.on("mouseover", function(d, i) {
+		svg.append("text")
+		  .attr("dy", d3.mouse(this)[1])
+		  .attr("dx", d3.mouse(this)[0])
+		  .style("text-anchor", "middle")
+		  .style("font-size", 20)
+		  .attr("class","label")
+		  .style("fill", "white")
+		  .text(d.data.key)
+	})
+	.on("mouseout", () => svg.select(".label").remove())
+    .transition()
+    .duration(300)
+    .attr("stroke", "white")
+    .style("stroke-width", "2px")
+    .style("opacity", 1)
+	.attr('d', d3.arc()
+      .innerRadius(0)
+      .outerRadius(radius)
+    )
+    .attr('fill', (d) => color(d.data.key))
+
+	g
+    .exit()
+    .remove()
+}
+
+$("#data1").click(function () {
+	update(pieSet1)
+	$(this).prop("disabled", true)
+	$("#data2").prop("disabled", false)
+})
+
+$("#data2").click(function () {
+	update(pieSet2)
+	$(this).prop("disabled", true)
+	$("#data1").prop("disabled", false)
+})
---
+++ src/index.js
@@ -0,0 +1,146 @@
+const excelToJson = require('convert-excel-to-json')
+const fs = require('fs')
+const path = require('path')
+const express = require('express')
+const R = require('ramda')
+
+sourceFile = './COVID/export_dataframe_medium.xlsx'
+
+// INFECTED
+const infected = excelToJson({
+    sourceFile: sourceFile,
+    header: {
+        rows: 1
+    },
+    sheets: ['Sheet1'],
+    columnToKey: {
+        AI: 'Contagiado',
+        G: 'Estado',
+        K: 'Fecha',
+        V: 'Asma',
+        O: 'Edad',
+        M: 'Intubado',
+        N: 'Neumonia'
+    }
+})
+
+fs.writeFileSync("src/data/infected.json", JSON.stringify(infected, null, 2), (err) => {
+    if (err) {
+        console.log(err)
+    }
+})
+
+//INFECTED BY STATE
+const infectedByState = infected.Sheet1.map(d => {
+    return {
+        Estado: d.Estado,
+        Contagiado: d.Contagiado
+    }
+})
+
+//INFECTED BY DATE
+const infectedByDate = infected.Sheet1.map(d => {
+    return {
+        Fecha: d.Fecha,
+        Contagiado: d.Contagiado
+    }
+})
+
+//ASTHMATIC BY AGE
+const asthmaByAge = infected.Sheet1.map(d => {
+    return {
+        Edad: d.Edad,
+        Contagiado: d.Contagiado,
+        Asma: d.Asma
+    }
+})
+
+//NEUMONIA BY AGE
+const neumoniaByAge = infected.Sheet1.map(d => {
+    return {
+        Edad: d.Edad,
+        Neumonia: d.Neumonia,
+        Contagiado: d.Contagiado
+    }
+})
+
+//INTUBATED BY AGE
+const intubatedByAge = infected.Sheet1.map(d => {
+    return {
+        Edad: d.Edad,
+        Intubado: d.Intubado,
+        Contagiado: d.Contagiado
+    }
+})
+
+const app = express()
+const port = process.env.PORT || 3000
+
+app.use(express.json())
+//app.use('/', express.static(path.join(__dirname,  '../public')))
+
+app.get('/infectedByState', (req, res) => {
+    const getResult = R.countBy(R.prop('Estado'))
+
+    const filteredResult = infectedByState.filter(d => d.Contagiado.toLowerCase().includes("confirmado"))
+    
+    res.json(getResult(filteredResult))
+})
+
+app.get('/infectedByDate', (req, res) => {
+    const getResult = R.countBy(R.prop('Fecha'))
+
+    infectedByDate.forEach(obj => {
+        obj.Fecha = new Date(obj.Fecha).toLocaleDateString()
+    })
+
+    const filteredResult = infectedByDate.filter(d => d.Contagiado.toLowerCase().includes("confirmado"))
+    
+    res.json(getResult(filteredResult))
+})
+
+app.get('/asthmaByAge', (req, res) => {
+    const getResult = R.countBy(R.prop('Edad'))
+
+    const filteredResult = asthmaByAge.filter(d => {
+        var filtered = true
+        if(!d.Contagiado.toLowerCase().includes("confirmado")) filtered = false
+        if(d.Asma != 'SI') filtered = false
+
+        return filtered
+    })
+    
+    res.json(getResult(filteredResult))
+})
+
+app.get('/neumoniaByAge', (req, res) => {
+    const getResult = R.countBy(R.prop('Edad'))
+
+    const filteredResult = neumoniaByAge.filter(d => {
+        var filtered = true
+        if(!d.Contagiado.toLowerCase().includes("confirmado")) filtered = false
+        if(d.Neumonia != 'SI') filtered = false
+
+        return filtered
+    })
+    
+    res.json(getResult(filteredResult))
+})
+
+app.get('/intubatedByAge', (req, res) => {
+    const getResult = R.countBy(R.prop('Edad'))
+
+    const filteredResult = intubatedByAge.filter(d => {
+        var filtered = true
+        if(!d.Contagiado.toLowerCase().includes("confirmado")) filtered = false
+        if(d.Intubado != 'SI') filtered = false
+
+        return filtered
+    })
+    
+    res.json(getResult(filteredResult))
+})
+
+app.listen(port, () => {
+    console.log('Server is running on port ' + port)
+})
